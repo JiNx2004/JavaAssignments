a749068ed32ea8d71d6c85835ed71eff0e01d70f
Dhruv <dhruvarjun5@gmail.com>
2023-08-29 23:16:11 +0530

    v

---
+++ src/Week5/Q10.java
@@ -0,0 +1,21 @@
+//Program using function to count unique values in an array of 15 elements
+package Week5;
+
+import java.util.Scanner;
+
+public class Q10 {
+  void unique(int arr[]){
+
+  }  
+  public static void main(String args[]){
+    int arr[]
+    arr = new int[15];
+    Scanner sc = new Scanner(System.in);
+    for(int i = 0;i<15;i++){
+      System.out.println("Enter element "+i+":");
+      
+    }
+
+
+  }
+}
